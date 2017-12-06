f55e1d43025781b33a310d191924cab29e444252
asvmahesh1 <saidevopsaws@gmail.com>
2017-12-06 06:55:34 +0000

    first commit chef

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,6 +7,6 @@ node_name                "asvmahesh1"
 client_key               "#{current_dir}/asvmahesh1.pem"
 chef_server_url          "https://api.chef.io/organizations/cheftest33"
 cookbook_path            ["#{current_dir}/../cookbooks"]
-knife[:aws_access_key_id] = "AKIAJM2LLHVCZLJDK4VQ"
-knife[:aws_secret_access_key] = "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"
+knife[:aws_access_key_id] = AKIAJ5NFQRN33BCM5CCQ"
+knife[:aws_secret_access_key] = "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"
 knife[:supermarket_site] = 'https://supermarket.chef.io/users/asvmahesh1'
