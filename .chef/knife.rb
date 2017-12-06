83a0c55326f3a9a932629e2fb9fd5964e6020fff
asvmahesh1 <saidevopsaws@gmail.com>
2017-12-06 06:10:04 +0000

    first commit chef

---
+++ .chef/knife.rb
@@ -0,0 +1,12 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "asvmahesh1"
+client_key               "#{current_dir}/asvmahesh1.pem"
+chef_server_url          "https://api.chef.io/organizations/cheftest33"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:aws_access_key_id] = "AKIAJM2LLHVCZLJDK4VQ"
+knife[:aws_secret_access_key] = "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"
+knife[:supermarket_site] = 'https://supermarket.chef.io/users/asvmahesh1'
